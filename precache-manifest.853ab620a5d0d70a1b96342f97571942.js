628a5732f6784550cc20c7507b10eef6cd8baf65
Akelesis <marcus.tavares@outlook.com>
2020-01-06 20:52:44 -0300

    Primeiro Commit

---
+++ precache-manifest.853ab620a5d0d70a1b96342f97571942.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b752d35620253043681c66cd71eb2a49",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f88992629ee481befc1e",
+    "url": "/static/css/main.136b34f4.chunk.css"
+  },
+  {
+    "revision": "26d7fe814b23cd5a4776",
+    "url": "/static/js/2.c04ba422.chunk.js"
+  },
+  {
+    "revision": "c6d01cb19213691bc81a274434d00fe7",
+    "url": "/static/js/2.c04ba422.chunk.js.LICENSE"
+  },
+  {
+    "revision": "f88992629ee481befc1e",
+    "url": "/static/js/main.d09eef62.chunk.js"
+  },
+  {
+    "revision": "7df52723d36a62aa5396",
+    "url": "/static/js/runtime-main.3127cda7.js"
+  }
+]);
